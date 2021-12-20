8c48b939596641685299c7e25ac864f95d530001
Dmitriev.NN <Dmitriev.NN.138.gmail.com>
2021-12-20 10:23:34 +0300

    It works

---
+++ README.md
@@ -0,0 +1,9 @@
+﻿
+This is another study project that is similar to classic To-do-list.
+
+In this application we can manage employees list. We can set their salaries, change promotion status by clicking to the name of employee (add a star symbol), give them bonus by clicking to the cookie symbol, add / remove employees, edit their names and salaries. Also we can use filters on this list to find employee by it's name, find employees with promotion or salary rate more than $ 1000. The summary information about employees is shown in the header of applicatin interface, and refreshes when list is employees changing.
+
+This is another study project that is similar to classic To-do-list. In this application we can manage employees list. We can set their salaries, change promotion status by clicking to the name of employee (add a star symbol), give them bonus by clicking to the cookie symbol, add / remove employees, edit their names and salaries. Also we can use filters on this list to find employee by it's name, find employees with promotion or salary rate more than $ 1000.
+
+ The summary information about employees is shown in the header of applicatin interface, and refreshes when list is employees changing.
+
---
+++ src/components/app/app.js
@@ -0,0 +1,123 @@
+import { Component } from 'react';
+
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: 'John C.', salary: 800, increase: false, rise: true, id: 1 },
+        { name: 'Alex M.', salary: 1000, increase: true, rise: false, id: 2 },
+        { name: 'Carl W.', salary: 500, increase: false, rise: false, id: 3 },
+      ],
+      term: '',
+      filter: 'all',
+    };
+    this.maxId = 4;
+  }
+
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return { data: data.filter((el) => el.id !== id) };
+    });
+  };
+
+  addItem = (name, salary) => {
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      rise: false,
+      id: this.maxId++,
+    };
+    this.setState(({ data }) => {
+      const newArr = [...data, newItem];
+      return { data: newArr };
+    });
+  };
+
+  onToggleProp = (id, prop) => {
+    this.setState(({ data }) => ({
+      data: data.map((item) => {
+        if (item.id === id) {
+          return { ...item, [prop]: !item[prop] };
+        }
+        return item;
+      }),
+    }));
+  };
+
+  onSalaryChange = (id, newValue) => {
+    this.setState(({ data }) => {
+      const index = data.findIndex((el) => el.id === id);
+      const newEl = { ...data[id - 1] };
+      newEl.salary = +newValue;
+      const before = data.slice(0, index);
+      const after = data.slice(index + 1);
+      const newArray = [...before, newEl, ...after];
+      return { data: newArray };
+    });
+  };
+
+  searchEmp = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+    return items.filter((item) => {
+      return item.name.indexOf(term) > -1;
+    });
+  };
+
+  onUpdateSearch = (term) => {
+    this.setState({ term });
+  };
+
+  filterPost = (items, filter) => {
+    switch (filter) {
+      case 'rise':
+        return items.filter((item) => item.rise);
+      case 'moreThen1000':
+        return items.filter((item) => item.salary >= 1000);
+      default:
+        return items;
+    }
+  };
+
+  onFilterSelect = (filter) => {
+    this.setState({ filter });
+  };
+
+  render() {
+    const { data, term, filter } = this.state;
+    const employees = this.state.data.length;
+    const increased = this.state.data.filter((item) => item.increase).length;
+    const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+
+    return (
+      <div className='app'>
+        <AppInfo employees={employees} increased={increased} />
+
+        <div className='search-panel'>
+          <SearchPanel onUpdateSearch={this.onUpdateSearch} />
+          <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
+        </div>
+        <EmployeesList
+          data={visibleData}
+          onDelete={this.deleteItem}
+          onToggleProp={this.onToggleProp}
+          onSalaryChange={this.onSalaryChange}
+        />
+        <EmployeesAddForm onAdd={this.addItem} />
+      </div>
+    );
+  }
+}
+
+export default App;
